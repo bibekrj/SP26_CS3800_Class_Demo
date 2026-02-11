a14b303bec79e00d5ab12580a92d107ad0f33d2c
bibekrj <29625141+bibekrj@users.noreply.github.com>
2026-02-11 15:26:05 -0500

    Database Script

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE todo_db;
+
+
+USE todo_do;
+
+-- CREATE TABLE todos(
+-- id INT AUTO_INCREMENT PRIMARY KEY,
+-- task VARCHAR(255),
+-- completed BOOLEAN DEFAULT FALSE);
+
+
+
+INSERT INTO todos(task) VALUES
+("Demo MYSQL Connection"),
+("Complete Model"),
+("Complete Server");
