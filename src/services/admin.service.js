5b6e0de56b72959ff8b0dc3426affba1830f525c
bibekrj <29625141+bibekrj@users.noreply.github.com>
2026-05-18 10:29:16 -0400

    final commit

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -1,15 +1,14 @@
 import { User, Todo } from "../models/index.js";
 
-export async function getAllTodosService(){
-    return await Todo.findAll( {order: [["task_id", "ASC"]]});
+export async function getAllTodosService() {
+  return await Todo.findAll({ order: [["task_id", "ASC"]] });
 }
 
-export async function getAllUserService(){
-    return await User.findAll({
-            attributes:["user_id", "user_name", "user_email", "user_role",]
-        })
+export async function getAllUserService() {
+  return await User.findAll({
+    attributes: ["user_id", "user_name", "user_email", "user_role"],
+  });
 }
-
 
 // export async function createUserTodoAdminService(user){
 //      if(!task || typeof task !=="string" || task.trim()===""){
